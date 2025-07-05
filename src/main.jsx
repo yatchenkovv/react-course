126489cb7155e69caf3c4d99544c0eee92a15e0a
vasiliy <yatchenko.1990@mail.ru>
2025-07-05 14:03:35 +0500

    create resturants list view

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import { createRoot } from "react-dom/client";
+import { restaurants } from "../materials/mock";
+
+const root = document.getElementById("root");
+const reactRoot = createRoot(root);
+
+reactRoot.render(
+  <div>
+    {restaurants.map((resturant) => {
+      return (
+        <div key={resturant.id}>
+          <h2 style={{ color: "green" }}>{resturant.name}</h2>
+          <h3>Меню</h3>
+          {
+            <ul>
+              {resturant.menu.map((item) => (
+                <li key={item.id}> {item.name} </li>
+              ))}
+            </ul>
+          }
+          <h3>Отзывы</h3>
+          {
+            <ul>
+              {resturant.reviews.map((review) => (
+                <li type="none" key={review.id}>
+                  - {review.text}
+                </li>
+              ))}
+            </ul>
+          }
+          <hr />
+        </div>
+      );
+    })}
+  </div>
+);
