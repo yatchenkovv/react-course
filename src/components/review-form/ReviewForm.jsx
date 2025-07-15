4b93dd95f8464ab91ea14afed98923cf2bae2039
vasiliy <yatchenko.1990@mail.ru>
2025-07-15 07:52:36 +0500

    create use-form

--- src/components/review-form/ReviewForm.jsx
+++ src/components/review-form/ReviewForm.jsx
@@ -1,7 +1,5 @@
-import { useReducer } from "react";
-import { ReviewRatingCounter } from "../review-rating-counter/ReviewRatingCounter";
-import { useCounter } from "../counter/use-counter";
-import { useEffect } from "react";
+import { Counter } from "../counter/Counter";
+import { useForm } from "./use-form";
 
 const DEFAULT_STATE = {
   name: "",
@@ -9,38 +7,9 @@ const DEFAULT_STATE = {
   rating: 0,
 };
 
-const SET_NAME_ACTION = "setName";
-const SET_REVIEW_ACTION = "setReview";
-const SET_RATING_ACTION = "setRating";
-const SET_DEFAULT_STATE = "setDefaultState";
-
-const reducer = (state, { type, payload }) => {
-  switch (type) {
-    case SET_NAME_ACTION:
-      return { ...DEFAULT_STATE, name: payload };
-    case SET_REVIEW_ACTION:
-      return { ...state, text: payload };
-    case SET_RATING_ACTION:
-      return { ...state, rating: payload };
-    case SET_DEFAULT_STATE:
-      return { ...DEFAULT_STATE, rating: 0 };
-
-    default:
-      return state;
-  }
-};
-
 export const ReviewForm = () => {
-  const { value, increment, decrement, resetCounter } = useCounter({
-    min: 0,
-    max: 5,
-  });
-  const [form, dispatch] = useReducer(reducer, DEFAULT_STATE);
-  const { name, text } = form;
-
-  useEffect(() => {
-    dispatch({ type: SET_RATING_ACTION, payload: value });
-  }, [value]);
+  const { form, setName, setReview, incrementRating, decrementRating, clear } =
+    useForm();
 
   return (
     <form
@@ -55,29 +24,25 @@ export const ReviewForm = () => {
         <label style={{ fontWeight: "bold" }}>Имя</label>
         <input
           type="text"
-          value={name}
-          onChange={(event) => {
-            dispatch({ type: SET_NAME_ACTION, payload: event.target.value });
-          }}
+          value={form.name}
+          onChange={(event) => setName(event.target.value)}
         />
       </div>
       <div style={{ display: "flex", flexDirection: "column", gap: "3px" }}>
         <label style={{ fontWeight: "bold" }}>Отзыв</label>
         <input
           type="text"
-          value={text}
-          onChange={(event) => {
-            dispatch({ type: SET_REVIEW_ACTION, payload: event.target.value });
-          }}
+          value={form.text}
+          onChange={(event) => setReview(event.target.value)}
         />
       </div>
 
       <div style={{ display: "flex", flexDirection: "column", gap: "3px" }}>
         <label style={{ fontWeight: "bold" }}>Рейтинг</label>
-        <ReviewRatingCounter
-          onIncrement={increment}
-          onDecrement={decrement}
-          rating={value}
+        <Counter
+          onClickIncrement={incrementRating}
+          onClickDecrement={decrementRating}
+          value={form.rating}
         />
       </div>
 
@@ -91,10 +56,7 @@ export const ReviewForm = () => {
           border: "none",
           cursor: "pointer",
         }}
-        onClick={() => {
-          resetCounter();
-          dispatch({ type: SET_DEFAULT_STATE, payload: undefined });
-        }}
+        onClick={clear}
       >
         Очистить
       </button>
