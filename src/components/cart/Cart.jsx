9f2598b835ad635a4d11b182bf0b45a381d4e793
vasiliy <yatchenko.1990@mail.ru>
2025-07-25 17:32:32 +0500

    fix show cart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -2,20 +2,12 @@ import { useSelector } from "react-redux";
 import { selectCartItems } from "../../redux/entities/cart/slice";
 import { CartItem } from "./CartItem";
 import styles from "./Cart.module.css";
-import { useUser } from "../user-context/use-user";
 
 export const Cart = () => {
   const items = useSelector(selectCartItems);
-  const { userName } = useUser();
 
   if (items.length === 0) {
-    return (
-      <>
-        {userName && (
-          <span className={styles.emptyCart}>В корзине пусто...</span>
-        )}
-      </>
-    );
+    return <span className={styles.emptyCart}>В корзине пусто...</span>;
   }
   return (
     <div className={styles.cart}>
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,13 +2,17 @@ import { Header } from "../header/Header";
 import { Footer } from "../footer/Footer";
 import { ProgressBar } from "../progress-bar/ProgressBar";
 import { Cart } from "../cart/Cart";
+import { useUser } from "../user-context/use-user";
 
-export const Layout = ({ children }) => (
-  <>
-    <ProgressBar />
-    <Header />
-    <main>{children}</main>
-    <Cart />
-    <Footer />
-  </>
-);
+export const Layout = ({ children }) => {
+  const { userName } = useUser();
+  return (
+    <>
+      <ProgressBar />
+      <Header />
+      <main>{children}</main>
+      {userName ? <Cart /> : null}
+      <Footer />
+    </>
+  );
+};
