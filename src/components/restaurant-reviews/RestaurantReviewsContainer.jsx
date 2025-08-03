d57cdaa8921ee0646153795a7df12010869993e3
vasiliy <yatchenko.1990@mail.ru>
2025-08-03 14:28:51 +0500

    move request get usrs

--- src/components/restaurant-reviews/RestaurantReviewsContainer.jsx
+++ src/components/restaurant-reviews/RestaurantReviewsContainer.jsx
@@ -6,6 +6,7 @@ import { RestaurantReviews } from "./RestaurantReviews.jsx";
 import { useRequest } from "../../redux/hooks/use-request.js";
 import { getReviewsById } from "../../redux/entities/reviews/get-reviews-by-restaurant-id.js";
 import { IDLE, PENDING } from "../../constants/request-status.js";
+import { getUsers } from "../../redux/entities/users/get-users.js";
 
 export const RestaurantReviewsContainer = () => {
   const { restaurantId } = useParams();
@@ -13,10 +14,16 @@ export const RestaurantReviewsContainer = () => {
   const restaurant = useSelector((state) =>
     selectRestaurantById(state, restaurantId)
   );
+  const requestUsersStatus = useRequest(getUsers);
 
   const requestStatus = useRequest(getReviewsById, restaurantId);
 
-  if (requestStatus === IDLE || requestStatus === PENDING) {
+  if (
+    requestStatus === IDLE ||
+    requestStatus === PENDING ||
+    requestUsersStatus === IDLE ||
+    requestUsersStatus === PENDING
+  ) {
     return <h3>Загрузка отзывов...</h3>;
   }
 
